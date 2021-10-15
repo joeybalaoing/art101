a1bf6e78f8d27b2991303235aa7ef18bb874c27b
Joey Balaoing <jdbalaoi@ucsc.edu>
2021-10-15 12:20:30 -0700

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+// Summary: This is psuedocode for two player rock, paper, scissors game & everyday task.
+// Description: This lab was an intro to psuedocoding for beginner level programmers where we described how to "how to get an A" for class and how to play: Rock, Paper, Scissors.
+// link:
+// file:
+// author: Monique Salinas & Joey Balaoing
+// since 10/13/21
+
+  <pre><code>
+    //Determine player 1 and player 2
+    //Determine what you want to choose: rock, paper, or scissors
+    //Prompt the user to pick rock, paper, or scissors
+    //Accept user input
+    //Test the win or draw condition
+    //Determine winner
+    //Display winner
+  </pre></code>
