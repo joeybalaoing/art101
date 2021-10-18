dc27494b84ae7d5bb653e3218129a57df5057c8b
Joey Balaoing <jdbalaoi@ucsc.edu>
2021-10-18 10:24:02 -0700

    Lab 5 done
    java script output and css changes to border and picture floating aligned right side

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ *  Authors: Monique Salinas & Joey Balaoing <jdbalaoi@ucscs.edu>
+ *  Created: September 18
+ *  License: Public Domain
+*/
+
+// Define Variables
+make = "Lamborghini";
+model = "Aventador";
+color = "Rose Gold";
+year = 2021;
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
